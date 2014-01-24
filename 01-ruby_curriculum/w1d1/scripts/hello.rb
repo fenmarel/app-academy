0b8cc794afcfa1290073d399e636882349f75026
fenmarel <jonny.p.tatoes@gmail.com>
2014-01-23 21:07:40 -0800

    finished script exercises

---
+++ 01-ruby_curriculum/w1d1/scripts/hello.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+
+def say_hello(name)
+	puts "Hey there, #{name}!"
+end
+
+if $PROGRAM_NAME == __FILE__
+	name = gets.chomp
+	say_hello(name)
+end
---
+++ 01-ruby_curriculum/w1d1/scripts/script.rb
@@ -0,0 +1,4 @@
+#!/usr/bin/env ruby
+# allow executable with, chmod +x script.rb
+
+puts "Hello from Ruby!"
