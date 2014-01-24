a1fe8e5e2078bf95a4b1affddf6e9c2a1b265222
fenmarel <jonny.p.tatoes@gmail.com>
2014-01-23 20:51:46 -0800

    finished debugging exercise

---
+++ 01-ruby_curriculum/w1d1/debugging/primes.rb
@@ -0,0 +1,24 @@
+# require 'debugger'
+
+def prime?(num)
+  (2...num).each do |i|
+    if (num % i) == 0
+      return false
+    end
+  end
+  true
+end
+
+def primes(num_primes)
+  ps = []
+  num = 1
+  while ps.count < num_primes
+    ps << num if prime?(num)
+    num += 1
+  end
+  ps
+end
+
+if __FILE__ == $PROGRAM_NAME
+  puts primes(100)
+end
