748100c121a3719e644d7e62ea2b9c7621587bc7
fenmarel <jonny.p.tatoes@gmail.com>
2014-02-02 21:31:44 -0800

    finished recursion exercises

--- 01-ruby_curriculum/w1d6-7/recursion_exercises/lib/recursion_problems.rb
+++ 01-ruby_curriculum/w1d6-7/recursion_exercises/lib/recursion_problems.rb
@@ -2,7 +2,10 @@
 #the sum of the integers.
 
 def sum_recur(array)
+  return 0 if array.empty?
+  return array.first if array.length == 1
 
+  array.first + sum_recur(array.drop(1))
 end
 
 
@@ -10,7 +13,10 @@ end
 #determine whether or not the array contains a specific value.
 
 def includes?(array, target)
+  return false if array.empty?
+  return true if array.first == target
 
+  includes?(array.drop(1), target)
 end
 
 
@@ -18,7 +24,10 @@ end
 #solution to count the number of occurrences of a specific value.
 
 def num_occur(array, target)
-  
+  return 0 if array.empty?
+
+  found = array.first == target ? 1 : 0
+  found + num_occur(array.drop(1), target)
 end
 
 
@@ -26,7 +35,12 @@ end
 #determine whether or not two adjacent elements of the array add to 12.
 
 def add_to_twelve?(array)
- 
+  return false if array.count < 2
+
+  first, second = array.take(2)
+  return true if first + second == 12
+
+  add_to_twelve?(array.drop(1))
 end
 
 
@@ -34,7 +48,13 @@ end
 #determine if the array is sorted.
 
 def sorted?(array)
- 
+  return [] if array.empty?
+  return true if array.count < 2
+
+  first, second = array.take(2)
+  return false if first > second
+
+  sorted?(array.drop(1))
 end
 
 
@@ -42,6 +62,8 @@ end
 #reversed. Must use recursion. (Don't use any #reverse methods!)
 
 def reverse(number)
-  
+  return number if number < 10
+
+  ((number % 10).to_s + reverse(number / 10).to_s).to_i
 end
 
