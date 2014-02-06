378c599490d3f13c3c603a51785d3922ecdfd5b3
fenmarel <jonny.p.tatoes@gmail.com>
2014-02-05 21:06:35 -0800

    moved players to their own file, refactored chess.rb

--- 01-ruby_curriculum/w2d3/chess.rb
+++ 01-ruby_curriculum/w2d3/chess.rb
@@ -1,5 +1,6 @@
 require './chess_pieces'
 require './chessboard'
+require './chess_players'
 require 'colorize'
 require 'io/console'
 require 'yaml'
@@ -17,50 +18,76 @@ class Game
   def initialize
     @board = Board.new
     @player1 = ComputerPlayer.new(:white, @board)
-    @player2 = HumanPlayer.new(:black, @board)
+    @player2 = ComputerPlayer.new(:black, @board)
     @curr_player = @player1
     @piece_to_move = nil
+    @computer_names = %W(Bubba Stu Sue Frank Barbara Belle Cleatus)
   end
 
   def play
     puts "Would you like to load a game? (y/n)"
-    ans = gets.chomp!.downcase.split('')
+    ans = gets.chomp!.downcase
     if ans[0] == 'y'
-      puts "Enter filename:"
-      filename = gets.chomp!
-      contents = File.read(filename)
-      saved_game = YAML::load(contents)
-      @board = saved_game.board
-      @player1 = saved_game.player1
-      @player2 = saved_game.player2
-      @curr_player = saved_game.curr_player
+      load_game_prompts
     else
-      puts "Enter Player1's name: "
-      @player1.name = gets.chomp!
-      puts "Enter Player2's name: "
-      @player2.name = gets.chomp!
+      set_name(@player1)
+      set_name(@player2)
     end
 
+    redraw_board
     until @board.checkmate?(@curr_player.color)
-      begin
-        if @curr_player.is_a?(ComputerPlayer)
-          @curr_player.play_turn
-          @curr_player = @curr_player == @player1 ? @player2 : @player1
-        else
-          navigate_and_select
-        end
-        redraw_board
-      rescue StandardError => e
-        p e.message
-        return
-      end
+      alternate_turns
     end
 
-    system 'clear'
-    @board.show_board
-
+    redraw_board
     winner = @curr_player == @player1 ? @player2 : @player1
     puts "#{winner.name} wins!"
+  end
+
+  def set_name(player)
+    if player.is_a?(HumanPlayer)
+      puts "Enter Player's name: "
+      player.name = gets.chomp!
+    else
+      player.name = "Computer #{@computer_names.shuffle.pop}"
+    end
+
+    self
+  end
+
+  def load_game_prompts
+    puts "Enter filename:"
+    filename = gets.chomp!
+    contents = File.read(filename)
+    saved_game = YAML::load(contents)
+
+    @board = saved_game.board
+    @player1 = saved_game.player1
+    @player2 = saved_game.player2
+    @curr_player = saved_game.curr_player
+
+    self
+  end
+
+  def alternate_turns
+    begin
+      if @curr_player.is_a?(ComputerPlayer)
+        @curr_player.play_turn
+        switch_player
+      else
+        navigate_and_select
+      end
+      redraw_board
+    rescue StandardError => e
+      p e.message
+      return
+    end
+  end
+
+  def switch_player
+    @curr_player = @curr_player == @player1 ? @player2 : @player1
+
+    self
   end
 
   def navigate_and_select
@@ -82,15 +109,7 @@ class Game
         elsif char == 'd'
           move(:right)
         elsif char == ' '
-          if !@piece_to_move.nil?
-            @curr_player.play_turn(@piece_to_move, @board.cursor)
-            @curr_player = @curr_player == @player1 ? @player2 : @player1
-            @piece_to_move = nil
-            @board.highlighted = []
-          elsif !@board[@board.cursor].nil?
-            @piece_to_move = @board.cursor
-            @board.highlighted = @board[@piece_to_move].valid_moves(@board)
-          end
+          lift_or_drop_piece
         elsif char == 'q'
           File.open(save_as, 'w') { |f| f.puts self.to_yaml }
         end
@@ -101,6 +120,20 @@ class Game
         retry
       end
     end
+  end
+
+  def lift_or_drop_piece
+    if !@piece_to_move.nil?
+      @curr_player.play_turn(@piece_to_move, @board.cursor)
+      switch_player
+      @piece_to_move = nil
+      @board.highlighted = []
+    elsif !@board[@board.cursor].nil?
+      @piece_to_move = @board.cursor
+      @board.highlighted = @board[@piece_to_move].valid_moves(@board)
+    end
+
+    self
   end
 
   def redraw_board
@@ -124,86 +157,6 @@ class Game
     elsif direction == :right
       @board.cursor = [(@board.cursor[0]), ((@board.cursor[1]+1) % 8)]
     end
-  end
-end
-
-class Player
-  attr_reader :color
-  attr_accessor :name
-
-  def initialize(color, board)
-    @name = nil
-    @color = color
-    @board = board
-  end
-end
-
-class HumanPlayer < Player
-  def play_turn(start_pos, end_pos)
-    raise InvalidMoveError if start_pos[1].nil? || start_pos[0].nil?
-    raise InvalidMoveError unless @board.get_pieces(@color).include?(@board[start_pos])
-    raise InvalidMoveError if end_pos[1].nil? || end_pos[0].nil?
-
-    @board.move(start_pos, end_pos)
-  end
-end
-
-class ComputerPlayer < Player
-  def generate_move
-    final_scores = []
-    our_pieces = @board.get_pieces(@color)
-    score = @board.score(@color)
-
-    our_pieces.shuffle.each do |piece|
-      possible_moves = piece.valid_moves(@board)
-      curr_distance = distance_from_enemy_king(piece.pos)
-
-      possible_moves.each do |possible_finish|
-        next_board = @board.dup
-        next_board.move(piece.pos, possible_finish)
-        next_score = next_board.score(@color)
-        next_score += 1 if distance_from_enemy_king(possible_finish) < curr_distance
-
-        if next_board.checkmate?(@color == :white ? :black : :white)
-          return [piece.pos, possible_finish]
-        end
-
-        enemy_pieces = next_board.get_pieces(@color == :white ? :black : :white)
-
-        enemy_pieces.each do |enemy_piece|
-          enemy_moves = enemy_piece.valid_moves(next_board)
-          enemy_moves.each do |enemy_move|
-            enemy_board = next_board.dup
-            enemy_board.move(enemy_piece.pos, enemy_move)
-            enemy_move_score = enemy_board.score(@color)
-
-            if enemy_move_score < next_score
-              final_score =  score - next_score
-            else
-              final_score = score - enemy_move_score
-            end
-
-            final_scores << [final_score, [piece.pos, possible_finish]]
-          end
-        end
-      end
-    end
-
-    final_scores.sort_by { |score, _| score }.first[1]
-  end
-
-  def distance_from_enemy_king(pos)
-    enemy_color = @color == :white ? :black : :white
-    king_pos = @board.find_king(enemy_color)
-
-    a2 = (king_pos[0] - pos[0]) ** 2
-    b2 = (king_pos[1] - pos[1]) ** 2
-
-    (a2 + b2) ** (1.0/2)
-  end
-
-  def play_turn
-    @board.move(*generate_move)
   end
 end
 
---
+++ 01-ruby_curriculum/w2d3/chess_players.rb
@@ -0,0 +1,83 @@
+class Player
+  attr_reader :color
+  attr_accessor :name
+
+  def initialize(color, board)
+    @name = nil
+    @color = color
+    @board = board
+  end
+
+  def other_color(color)
+    color == :white ? :black : :white
+  end
+end
+
+class HumanPlayer < Player
+  def play_turn(start_pos, end_pos)
+    raise InvalidMoveError if start_pos[1].nil? || start_pos[0].nil?
+    raise InvalidMoveError unless @board.get_pieces(@color).include?(@board[start_pos])
+    raise InvalidMoveError if end_pos[1].nil? || end_pos[0].nil?
+
+    @board.move(start_pos, end_pos)
+  end
+end
+
+class ComputerPlayer < Player
+  def generate_move
+    final_scores = []
+    our_pieces = @board.get_pieces(@color)
+    score = @board.score(@color)
+
+    our_pieces.shuffle.each do |piece|
+      possible_moves = piece.valid_moves(@board)
+      curr_distance = distance_from_enemy_king(piece.pos)
+
+      possible_moves.each do |possible_finish|
+        next_board = @board.dup
+        next_board.move(piece.pos, possible_finish)
+        next_score = next_board.score(@color)
+        next_score += 1 if distance_from_enemy_king(possible_finish) < curr_distance
+
+        if next_board.checkmate?(other_color(@color))
+          return [piece.pos, possible_finish]
+        end
+
+        enemy_pieces = next_board.get_pieces(other_color(@color))
+
+        enemy_pieces.each do |enemy_piece|
+          enemy_moves = enemy_piece.valid_moves(next_board)
+          enemy_moves.each do |enemy_move|
+            enemy_board = next_board.dup
+            enemy_board.move(enemy_piece.pos, enemy_move)
+            enemy_move_score = enemy_board.score(@color)
+
+            if enemy_move_score < next_score
+              final_score =  score - next_score
+            else
+              final_score = score - enemy_move_score
+            end
+
+            final_scores << [final_score, [piece.pos, possible_finish]]
+          end
+        end
+      end
+    end
+
+    final_scores.sort_by { |score, _| score }.first[1]
+  end
+
+  def distance_from_enemy_king(pos)
+    enemy_color = other_color(@color)
+    king_pos = @board.find_king(enemy_color)
+
+    a2 = (king_pos[0] - pos[0]) ** 2
+    b2 = (king_pos[1] - pos[1]) ** 2
+
+    (a2 + b2) ** (1.0/2)
+  end
+
+  def play_turn
+    @board.move(*generate_move)
+  end
+end
