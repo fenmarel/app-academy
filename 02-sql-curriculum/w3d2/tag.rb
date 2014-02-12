3e3a2e57f207b7a3b9538bfa28f03c0ee502f3f0
fenmarel <jonny.p.tatoes@gmail.com>
2014-02-11 21:00:46 -0800

    cleaned up tag.rb

--- 02-sql-curriculum/w3d2/tag.rb
+++ 02-sql-curriculum/w3d2/tag.rb
@@ -1,29 +1,9 @@
 require_relative 'questiondb'
 require_relative 'dbobject'
+require_relative 'question'
 
 class Tag < DBObject
-
-
-  def self.most_popular(n)
-
-    query = <<-SQL
-    SELECT
-    tags.*
-    FROM
-    tags LEFT OUTER JOIN question_tags ON tags.id = question_tags.tag_id
-    LEFT OUTER JOIN question_likes ON question_tags.question_id = question_likes.question_id
-    GROUP BY
-    tags.id
-    ORDER BY
-    COUNT(question_likes.question_id) DESC;
-    SQL
-
-    QuestionDB.instance.execute(query).map do |tag|
-      Tag.new(tag)
-    end.shift(n)
-
-  end
-
+  attr_reader :tag_name
 
   def initialize(options = {})
     @id = options['id']
@@ -32,31 +12,52 @@ class Tag < DBObject
     @table = 'tags'
   end
 
+  def self.most_popular(n)
+    query = <<-SQL
+    SELECT
+      tags.*
+    FROM
+      tags LEFT OUTER JOIN question_tags
+        ON tags.id = question_tags.tag_id
+          LEFT OUTER JOIN question_likes
+            ON question_tags.question_id = question_likes.question_id
+    GROUP BY
+      tags.id
+    ORDER BY
+      COUNT(question_likes.question_id) DESC;
+    SQL
+
+    QuestionDB.instance.execute(query).map do |tag|
+      Tag.new(tag)
+    end.shift(n)
+  end
+
   def params
     [@tag_name]
   end
 
-
   def most_popular_questions(n)
     query = <<-SQL
     SELECT
-    tagquest.*
+      tagquest.*
     FROM
-    ( SELECT
-    questions.*
-    FROM
-    questions JOIN question_tags ON question_tags.question_id = questions.id
-    WHERE
-  question_tags.tag_id = ? ) AS tagquest JOIN question_likes ON tagquest.id = question_likes.question_id
-  GROUP BY
-    tagquest.id
-  ORDER BY count(question_likes.user_id) DESC;
+      ( SELECT
+        questions.*
+      FROM
+        questions JOIN question_tags
+          ON question_tags.question_id = questions.id
+      WHERE
+        question_tags.tag_id = ? ) AS tagquest
+          JOIN question_likes
+            ON tagquest.id = question_likes.question_id
+    GROUP BY
+      tagquest.id
+    ORDER BY
+      count(question_likes.user_id) DESC;
     SQL
 
     QuestionDB.instance.execute(query, @id).map do |question|
       Question.new(question)
     end.shift(n)
-
   end
-
 end
