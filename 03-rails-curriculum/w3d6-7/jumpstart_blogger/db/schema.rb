3190eb7249a93ca82c98cc7c4f0d982e457112d4
fenmarel <jonny.p.tatoes@gmail.com>
2014-02-15 14:21:42 -0800

    switched db to sqlite3 as per instruction

--- 03-rails-curriculum/w3d6-7/jumpstart_blogger/db/schema.rb
+++ 03-rails-curriculum/w3d6-7/jumpstart_blogger/db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20140215214822) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "articles", force: true do |t|
     t.string   "title"
     t.text     "body"
