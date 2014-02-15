cc20656c7f9503fac93d7be4d294a9fb65107f46
fenmarel <jonny.p.tatoes@gmail.com>
2014-02-14 16:14:14 -0800

    associatable part a, complete

--- 02-sql-curriculum/w3d5/active_record_lite/lib/active_record_lite/04_associatable.rb
+++ 02-sql-curriculum/w3d5/active_record_lite/lib/active_record_lite/04_associatable.rb
@@ -10,23 +10,37 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    @name.singularize.camelize.constantize
   end
 
   def table_name
-    # ...
+    model_class.table_name
+  end
+
+  def update_params(options = {})
+    options.each do |k, v|
+      self.instance_variable_set("@#{k}", v)
+    end
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    @name = name
+    @class_name = @name.singularize.camelize
+    @primary_key = :id
+    @foreign_key = "#{@name}_id".to_sym
+    update_params(options)
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    @name = name
+    @class_name = @name.singularize.camelize
+    @primary_key = :id
+    @foreign_key = "#{self_class_name.underscore}_id".to_sym
+    update_params(options)
   end
 end
 
@@ -46,5 +60,5 @@ module Associatable
 end
 
 class SQLObject
-  # Mixin Associatable here...
+  extend Associatable
 end
