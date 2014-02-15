a3b84529e9b3dcb88876a6e01c5e6b133c7bb85e
fenmarel <jonny.p.tatoes@gmail.com>
2014-02-15 12:53:05 -0800

    initial commit for jumpstartlab's blogger 2

---
+++ 03-rails-curriculum/w3d6-7/jumpstart_blogger/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JumpstartBlogger::Application.config.secret_key_base = 'eb40650074a8ea5a65042e2b2fa84710eea8e3ec4b8e5ec3de98761b6e24737a9e35c6546c32e2bcc3ee70b80fcee68712d4dd10a0b57fa7f939715ffa389d35'
