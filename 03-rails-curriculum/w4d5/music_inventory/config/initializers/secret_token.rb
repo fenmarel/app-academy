0ad80840738b3d4dd25bf5161e22ef9577ff927d
fenmarel <jonny.p.tatoes@gmail.com>
2014-02-21 10:29:59 -0800

    initial commit for music_inventory

---
+++ 03-rails-curriculum/w4d5/music_inventory/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MusicInventory::Application.config.secret_key_base = '04829bb59457c9eb9a4dfd503140c0fe96ed51fe04a1f30169fbb4e5b2798dfa19a3c1f03c763c986f16c9e556931bc9ccbbefe030a447c8d13dd0549859df19'
