def7fc93d1d7981561df6e2c982ff2c304e0d252
fenmarel <jonny.p.tatoes@gmail.com>
2014-03-09 16:16:13 -0700

    phase 5, secret posts via ajax requests on user show page

---
+++ 04-js-curriculum/w6d5/SecretShareAjax/app/assets/javascripts/secrets.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $('#secret-form-container').on('submit', '#secret-form', function(event) {
+    event.preventDefault();
+
+    var $form = $(this);
+    var formData = $form.serializeJSON();
+
+    $.ajax({
+      url: '/secrets.json',
+      type: 'POST',
+      data: formData,
+      success: function(secret) {
+        $('#secret-listing').prepend('<li>' + secret.title + '</li>');
+        $('#secret-form input[type="text"]').val('');
+      }
+    })
+  })
+})
--- 04-js-curriculum/w6d5/SecretShareAjax/app/controllers/secrets_controller.rb
+++ 04-js-curriculum/w6d5/SecretShareAjax/app/controllers/secrets_controller.rb
@@ -7,11 +7,18 @@ class SecretsController < ApplicationController
   def create
     @secret = current_user.authored_secrets.build(secret_params)
 
-    if @secret.save
-      redirect_to user_url(secret_params[:recipient_id])
-    else
-      flash.now[:errors] = @secret.errors.full_messages
-      render :new
+    respond_to do |format|
+      if @secret.save
+        format.html { redirect_to user_url(secret_params[:recipient_id]) }
+        format.json { render :json => @secret }
+      else
+        format.html do
+          flash.now[:errors] = @secret.errors.full_messages
+          render :new
+        end
+
+        format.json { render :json => @secret.errors, status: 422 }
+      end
     end
   end
 
